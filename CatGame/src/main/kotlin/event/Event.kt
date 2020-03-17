e4edbcb0b21ce75a88a9d95508de2e7ed80ffdd7
Darya Razumova <d.razumova@yandex.ru>
2020-03-18 00:08:41 +0300

    add EventBus

--- CatGame/src/main/kotlin/event/Event.kt
+++ CatGame/src/main/kotlin/event/Event.kt
@@ -15,31 +15,6 @@ class PlayerEvent(private val queue: String) : Event { // TODO tut
     }
 }
 
-class NewLevelEvent() : Event {
-    override fun apply(context: Context): EventResult {
-        val playerPoint = context.getPlayerPoint() ?: return EventResult(ExitCode.EXIT, "No player found on the map")
-        if (context.containsClass(DoorDown::class, playerPoint)) {
-            return EventResult(ExitCode.GO_DOWN)
-        }
-        if (context.containsClass(DoorUp::class, playerPoint)) {
-            return EventResult(ExitCode.GO_UP)
-        }
-        return EventResult(ExitCode.CONTINUE)
-    }
-}
-
-class PickClewEvent() : Event {
-    override fun apply(context: Context): EventResult {
-        val playerPoint = context.getPlayerPoint() ?: return EventResult(ExitCode.EXIT, "No player found on the map")
-        if (context.containsClass(Clew::class, playerPoint)) {
-            context.removeObject(Clew::class, playerPoint)
-            val player = context.getPlayer()
-            player?.addClew()
-        }
-        return EventResult(ExitCode.CONTINUE)
-    }
-}
-
 enum class ExitCode {
     CONTINUE, GO_UP, GO_DOWN, EXIT
 }
