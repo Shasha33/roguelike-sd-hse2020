d3af0374fece5b2015c3498891980a7a3a605bdf
Darya Razumova <d.razumova@yandex.ru>
2020-03-18 05:15:34 +0300

    level saving and loading

--- CatGame/src/main/kotlin/logic/LevelLoader.kt
+++ CatGame/src/main/kotlin/logic/LevelLoader.kt
@@ -1,13 +1,29 @@
 package logic
 
-import data.Context
+import com.google.gson.GsonBuilder
+import data.*
+import java.io.File
 
-class LevelLoader { // TODO aga
+class LevelLoader {
+    private val gson = GsonBuilder()
+        .registerTypeAdapter(DoorUp::class.java, GameObjectSerializer())
+        .registerTypeAdapter(DoorDown::class.java, GameObjectSerializer())
+        .registerTypeAdapter(Wall::class.java, GameObjectSerializer())
+        .registerTypeAdapter(Player::class.java, GameObjectSerializer())
+        .registerTypeAdapter(Clew::class.java, GameObjectSerializer())
+        .create()
+
     fun saveTo(context: Context, path: String) {
-
+        val jsonText = gson.toJson(context)
+        val file = File(path)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        file.writeText(jsonText)
     }
 
     fun loadFrom(path: String): Context {
-        return Context()
+        val text = File(path).readText()
+        return gson.fromJson(text, Context::class.java)
     }
 }
