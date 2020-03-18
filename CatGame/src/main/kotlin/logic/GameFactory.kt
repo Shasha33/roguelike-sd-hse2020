ac3df35669e3b3a2eaf0e73cfb5c26de0562456f
Darya Razumova <d.razumova@yandex.ru>
2020-03-18 06:24:24 +0300

    forgotten list in game factory

--- CatGame/src/main/kotlin/logic/GameFactory.kt
+++ CatGame/src/main/kotlin/logic/GameFactory.kt
@@ -4,6 +4,6 @@ import data.Context
 
 class GameFactory {
     fun createGame(context: Context): GameLoop {
-        return GameLoop(context)
+        return GameLoop(context, emptyList())
     }
 }
