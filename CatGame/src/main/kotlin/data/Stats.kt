5cbd08824e5b57814ba753b8e4589469fc3995c9
Darya Razumova <d.razumova@yandex.ru>
2020-04-29 02:07:33 +0300

    fixed stats comparison

--- CatGame/src/main/kotlin/data/Stats.kt
+++ CatGame/src/main/kotlin/data/Stats.kt
@@ -6,14 +6,14 @@ import kotlinx.serialization.Serializable
 data class Stats(var hp: Int, val attack: Int, val armor: Int) : Comparable<Stats> {
     override fun compareTo(other: Stats): Int {
         if (attack < other.attack) {
-            return -1
+            return 1
         }
         if (attack == other.attack) {
             if (armor < other.armor) {
-                return -1
+                return 1
             }
             return 0
         }
-        return 1
+        return -1
     }
 }
