606aebfcb60c7aefd348a480e3b95078714d2cad
Darya Razumova <d.razumova@yandex.ru>
2020-05-13 03:52:39 +0300

    changed to executor

--- CatGame/src/main/kotlin/grpc/Client.kt
+++ CatGame/src/main/kotlin/grpc/Client.kt
@@ -3,11 +3,14 @@ package grpc
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.asExecutor
 import kotlinx.coroutines.runBlocking
 import logic.LevelLoader
 import ru.hse.kek.KekGrpcKt
 import ru.hse.kek.Roguelike
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class Client(host: String, port: Int) {
     private val levelLoader = LevelLoader()
@@ -16,8 +19,8 @@ class Client(host: String, port: Int) {
 
     init {
         val channelBuilder = ManagedChannelBuilder.forAddress(host, port).usePlaintext()
-        val dispatcher = Dispatchers.Default
-        channel = channelBuilder.executor(dispatcher.asExecutor()).build()
+        val executor = Executors.newCachedThreadPool()
+        channel = channelBuilder.executor(executor).build()
     }
 
     private val stub = KekGrpcKt.KekCoroutineStub(channel)
